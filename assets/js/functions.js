5ab2f2cf973ebcaeec1a37b807eb4829807c838c
StevenFeng <StevenFeng1020@outlook.com>
2024-10-16 21:15:47 +1100

    init

---
+++ assets/js/functions.js
@@ -0,0 +1,79 @@
+var carousel = document.querySelector('.carousel_2');
+var cells = carousel.querySelectorAll('.carousel__cell');
+var cellCount; 
+var selectedIndex = 0;
+var cellWidth = carousel.offsetWidth;
+var cellHeight = carousel.offsetHeight;
+var isHorizontal = true;
+var rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
+var radius, theta;
+
+function rotateCarousel() {
+  var angle = theta * selectedIndex * -1;
+  carousel.style.transform = 'translateZ(' + -radius + 'px) ' + 
+    rotateFn + '(' + angle + 'deg)';
+}
+
+var prevButton = document.querySelector('.previous-button');
+prevButton.addEventListener( 'click', function() {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener( 'click', function() {
+  selectedIndex++;
+  rotateCarousel();
+});
+
+function changeCarousel() {
+  cellCount = 5;
+  
+  console.log(cellCount)
+  theta = 360 / cellCount;
+  var cellSize = isHorizontal ? cellWidth : cellHeight;
+  radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellCount ) );
+  for ( var i=0; i < cells.length; i++ ) {
+    var cell = cells[i];
+    if ( i < cellCount ) {
+      // visible cell
+      cell.style.opacity = 1;
+      var cellAngle = theta * i;
+      cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
+    } else {
+      // hidden cell
+      cell.style.opacity = 0;
+      cell.style.transform = 'none';
+    }
+  }
+
+  rotateCarousel();
+}
+
+var orientationRadios = document.querySelectorAll('input[name="orientation"]');
+( function() {
+  for ( var i=0; i < orientationRadios.length; i++ ) {
+    var radio = orientationRadios[i];
+    radio.addEventListener( 'change', onOrientationChange );
+  }
+})();
+
+function onOrientationChange() {
+  var checkedRadio = document.querySelector('input[name="orientation"]:checked');
+  isHorizontal = checkedRadio.value == 'horizontal';
+  rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
+  changeCarousel();
+}
+
+// set initials
+onOrientationChange();
+
+var video = document.getElementById("myvideo");
+
+function toggleControls() {
+  if (video.hasAttribute("controls")) {
+     video.removeAttribute("controls")   
+  } else {
+     video.setAttribute("controls","controls")   
+  }
+}
